b2b2aba866fc2e8af514582a0ab01d58e702ce0a
tejalkumbhar2001 <tejalkumbhar5258@gmail.com>
2024-06-04 15:24:21 +0530

    changes in requirement and setup files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in biometric_attendance/__init__.py
+# get version from __version__ variable in biometric_attendance/__init__.py 
 from biometric_attendance import __version__ as version
 
 setup(
