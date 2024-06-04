9451c69654d548e4eae0f00fb15e9106c6d79950
tejalkumbhar2001 <tejalkumbhar5258@gmail.com>
2024-06-04 16:47:16 +0530

    Created files

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in biometric_attendance/__init__.py 
+# newly added
 from biometric_attendance import __version__ as version
 
 setup(
