76bf01365f65a39e368fa7dd161997f549a9c855
tejalkumbhar2001 <tejalkumbhar5258@gmail.com>
2024-06-04 15:08:11 +0530

    setup and requirement files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in biometric_attendance/__init__.py
+from biometric_attendance import __version__ as version
+
+setup(
+	name="biometric_attendance",
+	version=version,
+	description="Biometric Attendance Sync Used for getting Data from Biometric Machine",
+	author="quantbit technologies pvt ltd",
+	author_email="contact@erpdata.in",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
+
